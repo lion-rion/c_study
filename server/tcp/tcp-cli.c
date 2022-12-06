53fd271fb2b8737872b36a9c493bc338cb8c8f42
lion-rion <lionact08@gmail.com>
2022-12-06 15:06:54 +0900

    🧹 cleanup

--- server/tcp/tcp-cli.c
+++ server/tcp/tcp-cli.c
@@ -8,6 +8,7 @@
 #include <arpa/inet.h>
 int main(int argc, char *argv[])
 {
+    char ip = "127.0.0.1";  //ip address
     struct sockaddr_in server;
     int sock;
     char buf[65536];
@@ -16,20 +17,19 @@ int main(int argc, char *argv[])
     sock = socket(AF_INET, SOCK_STREAM, 0);
     /* 接続先指定用構造体の準備 */
     server.sin_family = AF_INET;
-    server.sin_port = htons(4000);
-    /* 127.0.0.1 は localhost */
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr.s_addr);
+    server.sin_port = htons(4000); //port number
+    inet_pton(AF_INET, ip, &server.sin_addr.s_addr);
     connect(sock, (struct sockaddr *)&server, sizeof(server));
     char cmd[128];
     char *ptr;
     while (1)
     {
-        printf("送信するメッセージ(終了する場合はquit) : "); fgets(cmd, 128, stdin); //コマンドを入力させる
+        printf("Message (or quit）) : : "); fgets(cmd, 128, stdin); //コマンドを入力させる
         cmd[strlen(cmd) - 1] = '\0';  //\nを削除
         write(sock, cmd, strlen(cmd));
         if(strcmp(cmd,"quit")==0) {
             //終了
-            printf("終了します\n");
+            printf("Exit...\n");
             break;
         }
         
@@ -38,15 +38,11 @@ int main(int argc, char *argv[])
         read(sock, buf, sizeof(buf));
         if(strcmp(buf, "quit") == 0)
         {
-            printf("サーバーから切断されました\n");
+            printf("Disconnected\n");
             break;
         }
         
         printf("%s\n", buf);
-        //内部コマンドの判定
-        // if (check_cmd(cmd) == 1) {
-        //     return 0;
-        // }
     }
     
     /* socket の終了 */
